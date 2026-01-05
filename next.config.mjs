db9b7e07b43c5a079194fa5445f8788a1f72696f
Diwakar Sharma <diwakarsharma2216@gmail.com>
2026-01-05 12:18:56 +0530

    fix:2.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://gemviewonline.blr1.digitaloceanspaces.com/:path*',
-      },
-    ];
-  },
+ 
 };
 
 export default nextConfig;
